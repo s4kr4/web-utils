da276ef67903e8b5680df01de6adb0096fdb45fb
s4kr4 <s4kr4m4@gmail.com>
2023-09-18 00:27:09 +0900

    Update side menu

--- src/components/parts/SideMenu.tsx
+++ src/components/parts/SideMenu.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link';
 
 const menuList = [
-  { name: 'top', path: '/' },
+  { name: 'Top Page', path: '/' },
   { name: 'Lives', path: '/lives' },
 ];
 
 export const SideMenu = () => {
   return (
-    <nav className="h-screen w-72 bg-gray-900 text-white">
-      <ul className="p-5 pt-10">
+    <nav className="p-5 h-screen w-72 bg-gray-900 text-white">
+      <div className="text-2xl">Web Utils</div>
+      <ul className="pt-5">
         {menuList.map((menu) => (
           <li
             key={menu.name}
             className="p-4 rounded-md font-bold cursor-pointer hover:bg-gray-800"
           >
-            <Link href={menu.path}>{menu.name}</Link>
+            <Link href={menu.path}>- {menu.name}</Link>
           </li>
         ))}
       </ul>
